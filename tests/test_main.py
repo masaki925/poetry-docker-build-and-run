315ecc4b02215722ec5a725b605119570cfbb4a2
masaki925 <masaki925@gmail.com>
2022-09-05 14:02:13 +0900

    apply black formatter

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,6 +4,7 @@ from poetry_docker_build_and_run.main import app
 
 client = TestClient(app)
 
+
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
--- tests/test_poetry_docker_build_and_run.py
+++ tests/test_poetry_docker_build_and_run.py
@@ -2,4 +2,4 @@ from poetry_docker_build_and_run import __version__
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
